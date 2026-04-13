33f64890f4427793ebce20a6cc3dbe3dd4a34c98
Thallyson Moreira <thallysonfariaa@gmail.com>
2026-04-13 08:38:12 -0300

    Recuperando parte dos arquivos perdidos

---
+++ public/javascript/main.js
@@ -0,0 +1,19 @@
+// Impotations Gsaps
+import { gsap } from "./animations/gsap.js";
+import { ScrollTrigger } from "./animations/gsap.js";
+
+// Animations Gsaps
+import { entrace } from "./animations/header/entrace.js";
+import {scrollZoom} from "./animations/header/scroll_zoom.js"
+
+
+addEventListener("load", () => { 
+
+    const header = document.querySelector(".header");
+    const header_profile = document.querySelector("#header_profile");
+    const nav = document.querySelector("#navigation");
+    
+    scrollZoom(gsap,header);
+    entrace(gsap, header, header_profile, nav);
+    
+})

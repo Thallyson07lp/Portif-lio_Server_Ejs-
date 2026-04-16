f8771cc2aba7de342fd976c17c0427966114a1cc
Thallyson Moreira <thallysonfariaa@gmail.com>
2026-04-16 19:24:39 -0300

    Refactor: Skills.css

--- server.js
+++ server.js
@@ -2,12 +2,15 @@ import server, { port } from "./app.js"
 
 try {
 
-  await server.listen({ port });
+  await server.listen({
+    port,
+    host: "0.0.0.0"
+  });
   console.log(`Servidor na porta ${port} online...`);
 
 } catch (err) {
 
   server.log.error(err)
   process.exit(1)
-  
+
 }
